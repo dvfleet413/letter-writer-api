b986d2c66f83b6d30a628958f6dfe86a9249c8cf
dvfleet413 <dvfleet413@gmail.com>
2020-06-10 17:32:39 -0400

    added User model

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,6 @@
 class ContactsController < ApplicationController
     def index
         contacts = Contact.all
-        render json: contacts.to_json(only: [:name, :address, :phone])
+        render json: contacts.to_json(only: [:name, :address, :phone, :lat, :lng])
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_162809) do
+ActiveRecord::Schema.define(version: 2020_06_10_212936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2020_06_10_162809) do
 
   create_table "territories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
