59ed8639e086b4fe76c07bf7cc5a1911e14298bd
davevanfleet <vanfleet@arsome.com>
2021-08-05 10:09:22 -0400

    adding congregations endpoints

--- app/controllers/congregations_controller.rb
+++ app/controllers/congregations_controller.rb
@@ -2,6 +2,7 @@ class CongregationsController < ApplicationController
     def create
         cong = Congregation.new(congregation_params)
         if cong.save()
+            user = cong.users.build(user_params)
             render json: cong
         else
             render json: {error: "unable to save"}, status: :bad_request
@@ -9,7 +10,11 @@ class CongregationsController < ApplicationController
     end
 
     private
-        def congregation_params:
+        def congregation_params
             params.require(:congregation).permit(:name, :api_access)
+        end
+
+        def user_params
+            params.require(:user).permit(:name, :password, :congregation_id)
         end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
     def create
+        set_congregation
+        @congregation.users.build(user_params)
     end
 
     private
         def user_params
             params.require(:user).permit(:name, :password, :congregation_id)
+        end
+
+        def set_congregation
+            @congregation = Congregation.find(params[:congregation_id])
         end
 end
