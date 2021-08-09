c49b995ef4f627fbdf48451be31005e24fe7ce89
davevanfleet <vanfleet@arsome.com>
2021-08-09 19:00:30 -0400

    prepend api to all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
-  post '/login', to: "sessions#create"
-  post '/logout', to: "sessions#destroy"
-  get '/get_current_user', to: "sessions#get_current_user"
-  get '/get_daily_text', to: "text#todays_text"
-  resources :contacts, only: [:index, :create]
-  resources :congregations do
-    resources :users, only: [:index, :create]
-    resources :external_contacts, only: [:index, :create, :destroy]
-    resources :territories, only: [:create, :index, :show, :update] do 
-      resources :dncs
-      resources :assignments
-      resources :external_contacts, only: [:index]
+  scope '/api' do
+    post '/login', to: "sessions#create"
+    post '/logout', to: "sessions#destroy"
+    get '/get_current_user', to: "sessions#get_current_user"
+    get '/get_daily_text', to: "text#todays_text"
+    resources :contacts, only: [:index, :create]
+    resources :congregations do
+      resources :users, only: [:index, :create]
+      resources :external_contacts, only: [:index, :create, :destroy]
+      resources :territories, only: [:create, :index, :show, :update] do 
+        resources :dncs
+        resources :assignments
+        resources :external_contacts, only: [:index]
+      end
+      resources :dncs, only: [:index, :create]
     end
-    resources :dncs, only: [:index, :create]
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
