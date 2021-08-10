c491f6f791a9a95c1e58534379ead161b58a9124
davevanfleet <vanfleet@arsome.com>
2021-08-09 20:24:21 -0400

    adjusted from email so I dont forget later...

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-    default from: 'notifications@example.com'
+    default from: 'noreply@territorycounter.com'
 
     def confirmation_email
         @user = params[:user]
