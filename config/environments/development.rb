62333a2a0f0b5af057372557078e06e5dba7a784
davevanfleet <vanfleet@arsome.com>
2021-08-09 20:23:35 -0400

    started including action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,9 +29,19 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'mail.hover.com',
+    port:                 25,
+    domain:               'territorycounter.com',
+    user_name:            ENV["EMAIL_USERNAME"],
+    password:             ENV["EMAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
